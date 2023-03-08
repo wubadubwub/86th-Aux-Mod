49c0525d90a9625fec0b1ef4785660617b166d9e
Sinder86th <121121046+Sinder86th@users.noreply.github.com>
2023-03-08 01:35:21 -0600

    Oh yeah, it's all coming together
    Heimdall
    SMG
    SAW
    WARFARIN!

--- Addons/STB86_Auxiliary/armor/Config/config.cpp
+++ Addons/STB86_Auxiliary/armor/Config/config.cpp
@@ -1767,6 +1767,144 @@ class CfgWeapons
 			};
 		};
 	};
+	class STB86_Vandal_Heimdall_vest/*STB86_M52D_Armor*/ : STB86_M52D_Armor
+	{
+
+		displayName = "[86th STB] M52D Heimdall";
+
+
+
+		hiddenSelectionsTextures[] =
+		{
+			"OPTRE_UNSC_Units\Army\data\Vest_odst_co.paa",
+			"V_FZ_Armor\Data\Vests\V_M52_URB_CO.paa",
+			"STB86_Auxiliary\armor\Vandal_legs_co.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"STB86_Auxiliary\armor\Vandal_heimdall_armor_co.paa"
+		};
+
+		class ItemInfo : VestItem
+		{
+
+			vestType = "Rebreather";
+			uniformModel = "\OPTRE_UNSC_Units\Army\armor.p3d";
+
+			mass = 80;
+
+			modelSides[] = { 6 };
+
+			containerClass = "Supply100";
+
+			hiddenSelections[] =
+			{
+			"camo",
+			"camo2",
+			"camo3",
+			"camo4",
+			"camo5",
+			//"A_Base",
+			//"A_ChestArmor",
+			"A_Ghillie",
+			//"A_KneesLeft",
+			//"A_KneesRight",
+			"A_KneesMarLeft",
+			"A_KneesMarRight",
+			//"A_ODST",
+			//"A_ShinArmorLeft",
+			//"A_ShinArmorRight",
+			//"A_TacPad",
+			//"A_ThighArmorLeft",
+			//"A_ThighArmorRight",
+			"AS_BaseLeft",
+			"AS_BaseRight",
+			"AS_LargeLeft",
+			"AS_LargeRight",
+			"AS_MediumLeft",
+			"AS_MediumRight",
+			"AS_ODSTCQBLeft",
+			"AS_ODSTCQBRight",
+			"AS_ODSTLeft",
+			//"AS_ODSTRight",
+			//"AS_ODSTSniperLeft",
+			"AS_ODSTSniperRight",
+			//"AS_SmallLeft",
+			"AS_SmallRight",
+			"AP_AR",
+			"AP_BR",
+			//"AP_Canteen",
+			"AP_GL",
+			"AP_Knife",
+			"AP_MGThigh",
+			"AP_AR",
+			"AP_Pack",
+			//"AP_Pistol",
+			"AP_Rounds",
+			"AP_SG",
+			"AP_SMG",
+			//"AP_Sniper",
+			"AP_Thigh",
+			"AP_Frag",
+			"AP_Smoke",
+			"APO_AR",
+			"APO_BR",
+			"APO_Knife",
+			"APO_SMG",
+			//"APO_Sniper",
+			"CustomKit_Scorch"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Neck
+				{
+					hitpointName = "HitNeck";
+					armor = 45;
+					passThrough = 0.1;
+				};
+				class Legs
+				{
+					hitpointName = "HitLegs";
+					armor = 45;
+					passThrough = 0.1;
+				};
+				class Arms
+				{
+					hitpointName = "HitArms";
+					armor = 45;
+					passThrough = 0.1;
+				};
+				class Hands
+				{
+					hitpointName = "HitHands";
+					armor = 45;
+					passThrough = 0.1;
+				};
+				class Chest
+				{
+					hitpointName = "HitChest";
+					armor = 45;
+					passThrough = 0.1;
+				};
+				class Diaphragm
+				{
+					hitpointName = "HitDiaphragm";
+					armor = 45;
+					passThrough = 0.1;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 45;
+					passThrough = 0.1;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					armor = 45;
+					passThrough = 0.1;
+				};
+			};
+		};
+	};
 	class STB86_Vandal_bow_vest/*STB86_M52D_Armor*/ : STB86_M52D_Armor
 	{
 
@@ -3266,282 +3404,6 @@ class CfgWeapons
 			//"APO_AR",
 			"APO_BR",
 			//"APO_Knife",
-			"APO_SMG",
-			"APO_Sniper",
-			"CustomKit_Scorch"
-			};
-			class HitpointsProtectionInfo
-			{
-				class Neck
-				{
-					hitpointName = "HitNeck";
-					armor = 45;
-					passThrough = 0.1;
-				};
-				class Legs
-				{
-					hitpointName = "HitLegs";
-					armor = 45;
-					passThrough = 0.1;
-				};
-				class Arms
-				{
-					hitpointName = "HitArms";
-					armor = 45;
-					passThrough = 0.1;
-				};
-				class Hands
-				{
-					hitpointName = "HitHands";
-					armor = 45;
-					passThrough = 0.1;
-				};
-				class Chest
-				{
-					hitpointName = "HitChest";
-					armor = 45;
-					passThrough = 0.1;
-				};
-				class Diaphragm
-				{
-					hitpointName = "HitDiaphragm";
-					armor = 45;
-					passThrough = 0.1;
-				};
-				class Abdomen
-				{
-					hitpointName = "HitAbdomen";
-					armor = 45;
-					passThrough = 0.1;
-				};
-				class Body
-				{
-					hitpointName = "HitBody";
-					armor = 45;
-					passThrough = 0.1;
-				};
-			};
-		};
-	};
-	class STB86_Firebrand_Joker_vest/*STB86_M52D_Armor*/ : STB86_M52D_Armor
-	{
-		displayName = "[86th STB] M52D Joker";
-		hiddenSelectionsTextures[] =
-		{
-			"OPTRE_UNSC_Units\Army\data\Vest_odst_co.paa",
-			"V_FZ_Armor\Data\Vests\V_M52_URB_CO.paa",
-			"STB86_Auxiliary\armor\Firebrand_Joker_legs_co.paa",
-			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
-			"STB86_Auxiliary\armor\Firebrand_Joker_armor_co.paa"
-		};
-
-		class ItemInfo : VestItem
-		{
-
-			vestType = "Rebreather";
-			uniformModel = "\OPTRE_UNSC_Units\Army\armor.p3d";
-
-			mass = 80;
-
-			modelSides[] = { 6 };
-
-			containerClass = "Supply100";
-
-			hiddenSelections[] =
-			{
-			"camo",
-			"camo2",
-			"camo3",
-			"camo4",
-			"camo5",
-			//"A_Base",
-			//"A_ChestArmor",
-			"A_Ghillie",
-			//"A_KneesLeft",
-			//"A_KneesRight",
-			"A_KneesMarLeft",
-			"A_KneesMarRight",
-			//"A_ODST",
-			//"A_ShinArmorLeft",
-			//"A_ShinArmorRight",
-			//"A_TacPad",
-			//"A_ThighArmorLeft",
-			//"A_ThighArmorRight",
-			"AS_BaseLeft",
-			"AS_BaseRight",
-			"AS_LargeLeft",
-			"AS_LargeRight",
-			"AS_MediumLeft",
-			"AS_MediumRight",
-			"AS_ODSTCQBLeft",
-			"AS_ODSTCQBRight",
-			//"AS_ODSTLeft",
-			//"AS_ODSTRight",
-			"AS_ODSTSniperLeft",
-			"AS_ODSTSniperRight",
-			"AS_SmallLeft",
-			"AS_SmallRight",
-			"AP_AR",
-			"AP_BR",
-			"AP_Canteen",
-			"AP_GL",
-			//"AP_Knife",
-			"AP_MGThigh",
-			"AP_AR",
-			"AP_Pack",
-			"AP_Pistol",
-			"AP_Rounds",
-			"AP_SG",
-			"AP_SMG",
-			"AP_Sniper",
-			"AP_Thigh",
-			"AP_Frag",
-			"AP_Smoke",
-			"APO_AR",
-			"APO_BR",
-			//"APO_Knife",
-			//"APO_SMG",
-			"APO_Sniper",
-			"CustomKit_Scorch"
-			};
-			class HitpointsProtectionInfo
-			{
-				class Neck
-				{
-					hitpointName = "HitNeck";
-					armor = 45;
-					passThrough = 0.1;
-				};
-				class Legs
-				{
-					hitpointName = "HitLegs";
-					armor = 45;
-					passThrough = 0.1;
-				};
-				class Arms
-				{
-					hitpointName = "HitArms";
-					armor = 45;
-					passThrough = 0.1;
-				};
-				class Hands
-				{
-					hitpointName = "HitHands";
-					armor = 45;
-					passThrough = 0.1;
-				};
-				class Chest
-				{
-					hitpointName = "HitChest";
-					armor = 45;
-					passThrough = 0.1;
-				};
-				class Diaphragm
-				{
-					hitpointName = "HitDiaphragm";
-					armor = 45;
-					passThrough = 0.1;
-				};
-				class Abdomen
-				{
-					hitpointName = "HitAbdomen";
-					armor = 45;
-					passThrough = 0.1;
-				};
-				class Body
-				{
-					hitpointName = "HitBody";
-					armor = 45;
-					passThrough = 0.1;
-				};
-			};
-		};
-	};
-	class STB86_Firebrand_diplex_vest/*STB86_M52D_Armor*/ : STB86_M52D_Armor
-	{
-
-
-
-
-
-		displayName = "[86th STB] M52D Diplex";
-
-
-
-		hiddenSelectionsTextures[] =
-		{
-			"OPTRE_UNSC_Units\Army\data\Vest_odst_co.paa",
-			"V_FZ_Armor\Data\Vests\V_M52_URB_CO.paa",
-			"STB86_Auxiliary\armor\Firebrand_legs_co.paa",
-			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
-			"STB86_Auxiliary\armor\Firebrand_diplex_armor_co.paa"
-		};
-
-		class ItemInfo : VestItem
-		{
-
-			vestType = "Rebreather";
-			uniformModel = "\OPTRE_UNSC_Units\Army\armor.p3d";
-
-			mass = 80;
-
-			modelSides[] = { 6 };
-
-			containerClass = "Supply100";
-
-			hiddenSelections[] =
-			{
-			"camo",
-			"camo2",
-			"camo3",
-			"camo4",
-			"camo5",
-			//"A_Base",
-			//"A_ChestArmor",
-			"A_Ghillie",
-			//"A_KneesLeft",
-			//"A_KneesRight",
-			"A_KneesMarLeft",
-			"A_KneesMarRight",
-			//"A_ODST",
-			//"A_ShinArmorLeft",
-			//"A_ShinArmorRight",
-			"A_TacPad",
-			//"A_ThighArmorLeft",
-			//"A_ThighArmorRight",
-			"AS_BaseLeft",
-			"AS_BaseRight",
-			"AS_LargeLeft",
-			"AS_LargeRight",
-			"AS_MediumLeft",
-			"AS_MediumRight",
-			"AS_ODSTCQBLeft",
-			"AS_ODSTCQBRight",
-			//"AS_ODSTLeft",
-			//"AS_ODSTRight",
-			"AS_ODSTSniperLeft",
-			"AS_ODSTSniperRight",
-			"AS_SmallLeft",
-			"AS_SmallRight",
-			"AP_AR",
-			"AP_BR",
-			//"AP_Canteen",
-			"AP_GL",
-			//"AP_Knife",
-			//"AP_MGThigh",
-			"AP_AR",
-			"AP_Pack",
-			"AP_Pistol",
-			"AP_Rounds",
-			"AP_SG",
-			"AP_SMG",
-			"AP_Sniper",
-			//"AP_Thigh",
-			"AP_Frag",
-			"AP_Smoke",
-			"APO_AR",
-			//"APO_BR",
-			"APO_Knife",
 			"APO_SMG",
 			"APO_Sniper",
 			"CustomKit_Scorch"
