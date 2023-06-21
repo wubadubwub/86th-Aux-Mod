788f0ec76b421d62705435b022def131c3d092f3
Sinder86th <121121046+Sinder86th@users.noreply.github.com>
2023-06-20 23:38:20 -0500

    Paradeus adjustments

--- Addons/Project_Paradeus/Music/config.cpp
+++ Addons/Project_Paradeus/Music/config.cpp
@@ -24,56 +24,72 @@ class CfgMusic
 	{
 		name = "[PD] Deadly Masks";  // Name of song in list
 		sound[] = {"Project_Paradeus\Music\Tracks\DeadlyMasks.ogg",1.2,1}; // File path and volume/speed
-		theme = "Project Paradeus"; // What category it goes in?
+		theme = "Project_Paradeus"; // What category it goes in?
 		duration = "233"; // Duration in seconds
-		musicClass = "Project Paradeus"; // Category
+		musicClass = "Project_Paradeus"; // Category
 	};
 	class PD2
 	{
 		name = "[PD] Deliver Death";
-		sound[] = {"Project_Paradeus\Music\Tracks\DeliverDeath.ogg",1.2,1};
-		theme = "Project Paradeus";
+		sound[] = {"Project_Paradeus\Music\Tracks\DeliverDeath.ogg",2.1,1};
+		theme = "Project_Paradeus";
 		duration = "134";
-		musicClass = "Project Paradeus";
+		musicClass = "Project_Paradeus";
 	};
 	class PD3
 	{
 		name = "[PD] Judge and Jury";
-		sound[] = {"Project_Paradeus\Music\Tracks\JudgeandJury.ogg",1.2,1};
-		theme = "Project Paradeus";
+		sound[] = {"Project_Paradeus\Music\Tracks\JudgeandJury.ogg",1.8,1};
+		theme = "Project_Paradeus";
 		duration = "193";
-		musicClass = "Project Paradeus";
+		musicClass = "Project_Paradeus";
 	};
 	class PD4
 	{
 		name = "[PD] Battle of Sisters";
-		sound[] = {"Project_Paradeus\Music\Tracks\Scarbattle.ogg",1.3,1};
-		theme = "Project Paradeus";
+		sound[] = {"Project_Paradeus\Music\Tracks\Scarbattle.ogg",1.4,1};
+		theme = "Project_Paradeus";
 		duration = "97";
-		musicClass = "Project Paradeus";
+		musicClass = "Project_Paradeus";
 	};
 	class PD5
 	{
 		name = "[PD] A memory forgotten";
 		sound[] = {"Project_Paradeus\Music\Tracks\ScarFeng.ogg",1.4,1};
-		theme = "Project Paradeus";
+		theme = "Project_Paradeus";
 		duration = "113";
-		musicClass = "Project Paradeus";
+		musicClass = "Project_Paradeus";
 	};
 	class PD6
 	{
 		name = "[PD] Another Era";
 		sound[] = {"Project_Paradeus\Music\Tracks\ScarMain.ogg",1.7,1};
-		theme = "Project Paradeus";
+		theme = "Project_Paradeus";
 		duration = "121";
-		musicClass = "Project Paradeus";
+		musicClass = "Project_Paradeus";
 	};
 	class PD7
 	{
 		name = "[PD] Tears and Casings";
 		sound[] = {"Project_Paradeus\Music\Tracks\ScarShen.ogg",1.7,1};
-		theme = "Project Paradeus";
+		theme = "Project_Paradeus";
 		duration = "92";
-		musicClass = "Project Paradeus";
+		musicClass = "Project_Paradeus";
+	};
+	class PD8
+	{
+		name = "[PD] A dark reality bore witness to";
+		sound[] = {"Project_Paradeus\Music\Tracks\DarkTruth.ogg",2.2,1};
+		theme = "Project_Paradeus";
+		duration = "169";
+		musicClass = "Project_Paradeus";
+	};
+	class PD9
+	{
+		name = "[PD] Forgotten husks of automatons";
+		sound[] = {"Project_Paradeus\Music\Tracks\ForgottenHusk.ogg",2,1};
+		theme = "Project_Paradeus";
+		duration = "195";
+		musicClass = "Project_Paradeus";
 	};
 }
