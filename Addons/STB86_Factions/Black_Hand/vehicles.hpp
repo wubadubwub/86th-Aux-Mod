1190a82e51b6507557b385d508e1e0649c529996
CatboyAJ <alsomjon@gmail.com>
2023-09-29 16:21:38 -0400

    Added Fuel and Ammo trucks for BH

--- Addons/STB86_Factions/Black_Hand/vehicles.hpp
+++ Addons/STB86_Factions/Black_Hand/vehicles.hpp
@@ -5,6 +5,8 @@ class O_LSV_02_AT_F;
 
 class O_truck_02_covered_F;
 class O_truck_02_transport_F; // A3_Soft_F_Exp_Truck_02
+class O_Truck_02_Ammo_F;
+class O_Truck_02_fuel_F;
 
 class STB86_BH_Car_unarmed : O_LSV_02_unarmed_F {
 	author = AUTHOR;
@@ -191,6 +193,77 @@ class STB86_BH_Truck_Covered : O_truck_02_covered_F {
 	hiddenSelectionsTextures[] = {"STB86_Factions\Black_Hand\textures\veh\Truck_01.paa","STB86_Factions\Black_Hand\textures\veh\Truck_02.paa","STB86_Factions\Black_Hand\textures\veh\Truck_03.paa"};
 
 };
+
+class STB86_BH_Truck_Ammo : O_Truck_02_Ammo_F {
+	author = AUTHOR;
+	dlc = DLC;
+	displayName = "Belly Ammo";
+	editorSubcategory = "STB86_BH_Veh_SubCat";
+	crew = "STB86_BH_Crewman";
+    faction = "STB86_Black_Hand";
+	class TextureSources
+	{
+		class STB86_BH_Truck_Tex
+		{
+			displayName="Black Hand";
+			author = AUTHOR;
+			dlc = DLC;
+			textures[]=
+			{
+				"STB86_Factions\Black_Hand\textures\veh\Truck_01.paa",
+				"STB86_Factions\Black_Hand\textures\Square.paa",
+				"STB86_Factions\Black_Hand\textures\veh\Truck_03.paa"
+			};
+			factions[]=
+			{
+				"STB86_Black_Hand"
+			};
+		};
+	};
+	textureList[]=
+	{
+		"STB86_BH_Truck_Tex",
+		1
+	};
+	hiddenSelectionsTextures[] = {"STB86_Factions\Black_Hand\textures\veh\Truck_01.paa","STB86_Factions\Black_Hand\textures\Square.paa","STB86_Factions\Black_Hand\textures\veh\Truck_03.paa"};
+
+};
+
+class STB86_BH_Truck_Fuel : O_Truck_02_fuel_F {
+	author = AUTHOR;
+	dlc = DLC;
+	displayName = "Belly Fuel";
+	editorSubcategory = "STB86_BH_Veh_SubCat";
+	crew = "STB86_BH_Crewman";
+    faction = "STB86_Black_Hand";
+	class TextureSources
+	{
+		class STB86_BH_Truck_Tex
+		{
+			displayName="Black Hand";
+			author = AUTHOR;
+			dlc = DLC;
+			textures[]=
+			{
+				"STB86_Factions\Black_Hand\textures\veh\Truck_01.paa",
+				"STB86_Factions\Black_Hand\textures\Square.paa",
+				"STB86_Factions\Black_Hand\textures\veh\Truck_03.paa"
+			};
+			factions[]=
+			{
+				"STB86_Black_Hand"
+			};
+		};
+	};
+	textureList[]=
+	{
+		"STB86_BH_Truck_Tex",
+		1
+	};
+	hiddenSelectionsTextures[] = {"STB86_Factions\Black_Hand\textures\veh\Truck_01.paa","STB86_Factions\Black_Hand\textures\Square.paa","STB86_Factions\Black_Hand\textures\veh\Truck_03.paa"};
+
+};
+
 
 class STB86_BH_CH_APC;
 class STB86_BH_CH_IFV;
